75117cf034d166c0a44bdbb55089af3b15a84355
Jmini392 <bread0902@gmail.com>
2025-09-09 13:19:50 +0900

    Drill1 제출 2022182010 김정민

---
+++ turtle_move.py
@@ -0,0 +1,28 @@
+import turtle
+
+def up():
+    turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
+def down():
+    turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
+def left():
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
+def right():
+    turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
+
+turtle.shape("turtle")
+turtle.stamp()
+turtle.onkey(up, 'w')
+turtle.onkey(down, 's')
+turtle.onkey(left, 'a')
+turtle.onkey(right, 'd')
+turtle.listen()
+turtle.mainloop()
+
